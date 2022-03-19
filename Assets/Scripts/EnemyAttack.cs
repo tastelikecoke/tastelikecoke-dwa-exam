151c8ee3abe61e3c5d1d9cb7fca72c4227478f6c
Jeru Mercado <jmerc87@gmail.com>
2022-03-20 00:02:56 +0800

    Add enemy bullet behaviour called EnemyAttack as a component

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float fireCooldown = 0.1f;
+    public float bulletSpeed = 0.05f;
+
+    [Header("Required References")]
+    public PlayerController targetPlayer = null;
+    public ProjectilePool projectilePool = null;
+    public GameObject mesh = null;
+
+    private float timeUntilFire = 0.0f;
+
+    private void Update()
+    {
+        Vector3 playerDirection = targetPlayer.transform.position -  transform.position;
+        
+        FireBullet(playerDirection);
+        if(timeUntilFire > 0.0f) timeUntilFire -= Time.deltaTime;
+    }
+    private void FireBullet(Vector3 inputDirection)
+    {
+        if(projectilePool != null && timeUntilFire <= 0.0f)
+        {
+            Projectile projectile = projectilePool.Spawn(this.transform.position, mesh.transform.rotation);
+            projectile.direction = inputDirection.normalized * bulletSpeed;
+            timeUntilFire = fireCooldown;
+        }
+    }
+}
