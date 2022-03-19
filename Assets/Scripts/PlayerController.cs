c279d3b0dcec600ea49b012922e28901eab13c6c
Jeru Mercado <jmerc87@gmail.com>
2022-03-19 11:37:58 +0800

    Add simple player movement controllers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 0.1f; 
+    private CharacterController characterController;
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Vector3 inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController?.Move(inputMovement * moveSpeed);
+    }
+}
